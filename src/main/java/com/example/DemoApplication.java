2faecb7c0cbc0c431a3e856dc41f743903fcbadb
Sandeep Kumar <119050419+sandeepkumar-dev@users.noreply.github.com>
2026-03-02 23:52:35 +0530

    Add variables for name and isSub in main method

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		String name="Sandeep";
+		boolean isSub= true;
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
