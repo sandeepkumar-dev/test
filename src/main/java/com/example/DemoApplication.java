81286e54b1ef62f2e394b1a49d51999a7f853934
sandeep. <kr.sandeep2112@gmail.com>
2026-03-02 23:56:30 +0530

    something added

--- src/main/java/com/example/DemoApplication.java
+++ src/main/java/com/example/DemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		int x = 100;
+		String name = "Kumar";
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
